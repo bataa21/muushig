2de85d5c190ff16d92b51a2c17ee30a6c66cdf01
bataa21 <76891522+bataa21@users.noreply.github.com>
2025-08-31 11:26:08 +0800

    Delete globals.js

--- globals.js
+++
@@ -1,5 +0,0 @@
-let suits = ['S', 'H', 'D', 'C'];
-let ranks = ['7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-let deck = [], playerHand = [], botHand = [], trumpCard = null;
-let playerScore = 15, botScore = 15;
-let currentPhase = 'swap';
